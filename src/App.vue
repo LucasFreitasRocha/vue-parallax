d5dfdb78c33f1360547e41216ea4785fdd43a7b9
Lucas Rocha <lucas@gigage.dev>
2020-08-28 12:17:11 -0300

    vue parallax

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app" class="container">
+    
+    <card
+      data-image="https://images-na.ssl-images-amazon.com/images/I/91M0isciFtL.jpg"
+    >
+      <h1 slot="header">Conspiracies</h1>
+      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </card>
+    <card
+      data-image="https://steamcdn-a.akamaihd.net/steam/apps/911400/header.jpg?t=1553879054"
+    >
+      <h1 slot="header">AC 3</h1>
+      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </card>
+    <card
+      data-image="https://4.bp.blogspot.com/-xeJ3Mx0Oyno/WodxowiQdhI/AAAAAAAAAw0/LTv7DWGBmo4T2gnmGtVmfagYopOnY8zBwCLcBGAs/w1200-h630-p-k-no-nu/assassin-s-creed-brasil-personagem-by-diogocarneir_njkc.1920.jpg"
+    >
+      <h1 slot="header">AC Brasil</h1>
+      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </card>
+    <card
+      data-image="https://cdn.gamer-network.net/2020/articles/2020-05-08-11-19/AC_Valhalla_logo.png/EG11/thumbnail/1920x1080/format/jpg/quality/80"
+    >
+      <h1 slot="header">Ac Valhalla</h1>
+      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
